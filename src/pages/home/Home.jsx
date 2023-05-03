c7ef50d3e4f944e033980c040b7b039390349106
soheldot <soheldot1239@gmail.com>
2023-05-04 02:22:34 +0600

    spacialrecipe section added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomeBanner from './HomeBanner';
 import AllChef from './AllChef';
+import SpacialRecipe from './SpacialRecipe';
 
 const Home = () => {
     return (
         <div>
             <HomeBanner></HomeBanner>
             <AllChef></AllChef>
+            <SpacialRecipe/>
         </div>
     );
 };
